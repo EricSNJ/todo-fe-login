81456ad7d37ada94c34fe2b0eda1573906972072
EricSNJ <145105956+EricSNJ@users.noreply.github.com>
2026-02-08 20:12:51 +0900

    page direction

--- src/App.js
+++ src/App.js
@@ -6,12 +6,35 @@ import TodoPage from "./pages/TodoPage";
 import RegisterPage from "./pages/RegisterPage";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  const getUser = async () => {
+    try {
+      const token = sessionStorage.getItem("token");
+      if (token) {
+        // 토큰이 있다면 서버에 사용자 정보를 요청
+        const response = await api.get("/user/me");
+        setUser(response.data.user);
+      }
+    } catch (error) {
+      console.log("사용자 정보 로딩 실패:", error.message);
+      setUser(null);
+    }
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
   return (
     <Routes>
-      <Route path="/" element={<TodoPage />} />
+
+      <Route
+      path="/" 
+        element={user ? <TodoPage user={user} setUser={setUser} /> : <Navigate to="/login" />} 
+        />
       <Route path="/register" element={<RegisterPage />} />
 
-      <Route path="/login" element={<LoginPage />} />
+      <Route path="/login" element={<LoginPage setUser={setUser}/>} />
     </Routes>
   );
 }
